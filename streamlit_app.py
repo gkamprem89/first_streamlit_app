b4b8687ccd2ce34e51898d0a6b200268b46fb51f
PREM KUMAR KARTHIKEYAN <gkam.prem89@gmail.com>
2023-11-21 10:48:16 +0530

    Update streamlit_app.py
    New header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
