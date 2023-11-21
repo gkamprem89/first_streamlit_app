5663dd6a2119a2cdaf564f4138e94d9fcc31be82
PREM KUMAR KARTHIKEYAN <gkam.prem89@gmail.com>
2023-11-21 10:49:02 +0530

    Update streamlit_app.py
    single app error correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
