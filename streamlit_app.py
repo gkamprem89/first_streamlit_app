e07feccaa244304ff94b4b5d63a844b452343cb9
PREM KUMAR KARTHIKEYAN <gkam.prem89@gmail.com>
2023-11-21 11:02:39 +0530

    Update streamlit_app.py
    Key error : Fruit correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list['fruit']))
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list['Fruit']))
 
 streamlit.dataframe(my_fruit_list)
